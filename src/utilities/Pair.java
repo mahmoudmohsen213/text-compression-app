6be85383871ee0e55665003d681e5c7e7c93a9bd
Mahmoud Mohsen <mahmoud.mohsen.213@outlook.com>
2015-03-01 20:51:09 +0200

    implementing generic pair

--- src/utilities/Pair.java
+++ src/utilities/Pair.java
@@ -1,45 +1,39 @@
 package utilities;
 
-public class Pair implements Comparable<Pair> {
 
-    public long first;
-    public String second;
+public class Pair<Type1 extends Comparable<Type1> ,Type2 extends Comparable<Type2> >
+		implements Comparable<Pair<Type1,Type2> > {
+
+    public Type1 first;
+    public Type2 second;
 
     public Pair() {}
     
-    public Pair(long first, String second) {
+    public Pair(Type1 first, Type2 second) {
         this.first = first;
         this.second = second;
     }
 
-    public void Copy(Pair p){
+    public void Copy(Pair<Type1,Type2> p){
     	this.first = p.first;
     	this.second = p.second;
     }
 
     @Override
-    public int compareTo(Pair p) {
-        if(this.first<p.first)  return -1;
-        if(this.first>p.first) return 1;
-        if(this.first==p.first){
-        	if(this.second.equals(p.second)) return 0;
-        	if(this.second.length()<p.second.length()) return -1;
-        	if(this.second.length()>p.second.length()) return 1;
-        	for(int i=0;i<this.second.length();i++){
-        		if(this.second.charAt(i)<p.second.charAt(i)) return -1;
-        		if(this.second.charAt(i)>p.second.charAt(i)) return 1;
-        	}
-        }
-        
-        return 0;
+    public int compareTo(Pair<Type1,Type2> p) {
+    	int firstComparison = this.first.compareTo(p.first);
+    	if(firstComparison != 0) return firstComparison;
+    	return this.second.compareTo(p.second);
     }    
-
-    @Override
+    
+	@Override
+    @SuppressWarnings("unchecked")
     public boolean equals(Object obj) {
-        if (!(obj instanceof Pair))
-            return false;
-        if (this == obj)
-            return true;
-        return ((this.first == ((Pair)obj).first ) && (this.second.equals(((Pair)obj).second)));
+		try{
+			if (this == obj) return true;
+        	return ((this.first.equals(((Pair<Type1,Type2>)obj).first)) &&
+        			(this.second.equals(((Pair<Type1,Type2>)obj).second)));
+		}catch(Exception e) { e.printStackTrace(); }
+		return false;
     }
 }
